37f5a78ab62caf04440b989f11bc3175a68fd965
Cravatec <samuel.wrobel@gmail.com>
2022-01-19 16:00:35 +0100

    Initial Commit

---
+++ OpenQuizz/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  OpenQuizz
+//
+//  Created by Sam on 19/01/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
