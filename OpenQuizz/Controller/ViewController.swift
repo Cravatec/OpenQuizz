5600f2493fd759133977e7e54fd8152d5d244721
Cravatec <samuel.wrobel@gmail.com>
2022-01-30 18:39:24 +0100

    Partie4

--- OpenQuizz/Controller/ViewController.swift
+++ OpenQuizz/Controller/ViewController.swift
@@ -9,6 +9,11 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var newGameButton: UIButton!
+    @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
+    @IBOutlet weak var scoreLabel: UILabel!
+    @IBOutlet weak var questionView: QuestionView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
---
+++ OpenQuizz/Model/Question.swift
@@ -0,0 +1,14 @@
+//
+//  Question.swift
+//  OpenQuizz
+//
+//  Created by Sam on 19/01/2022.
+//
+
+import Foundation
+
+struct Question {
+    var title = ""
+    var isCorrect = false
+}
+
